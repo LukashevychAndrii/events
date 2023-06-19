6d19a341f71dba20dca26f22345c61d022ecfbbf
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-06-19 21:54:26 +0300

    add demo home content

--- src/components/PagesComponents/Home/MainContent/MainContent.tsx
+++ src/components/PagesComponents/Home/MainContent/MainContent.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./MainContent.module.scss";
+import { motion } from "framer-motion";
 
 const MainContent = () => {
-  return <div>main content</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5, delay: 1.5 }}
+      className={styles["content"]}
+    >
+      <h1>text</h1>
+      <h2>text</h2>
+    </motion.section>
+  );
 };
 
 export default MainContent;
