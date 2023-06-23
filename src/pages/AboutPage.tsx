114d76594c5f3a6f0cedcead62545ef10f6bfe67
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-06-23 15:01:25 +0300

    remove motion component from aboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import About from "../components/PagesComponents/About/About";
-import { motion } from "framer-motion";
 
 const AboutPage = () => {
-  const AnimatedAbout = motion(About);
-  return <AnimatedAbout />;
+  return <About />;
 };
 
 export default AboutPage;
