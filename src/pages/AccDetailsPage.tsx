4486133b4df892cc23a54372ce05549b914d5eef
Andrii Lukashevych <andriilukashevych@gmail.com>
2023-07-21 17:17:27 +0300

    add redirect from acc details page
    add redirect from acc details page if user is not signed in

--- src/pages/AccDetailsPage.tsx
+++ src/pages/AccDetailsPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import AccDetails from "../components/AccDetails/AccDetails";
+import { useAuth } from "../hooks/use-auth";
+import { useNavigate } from "react-router-dom";
 
 const AccDetailsPage = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!auth.isAuth) {
+      navigate("/events/auth/sign-in");
+    }
+  }, [auth, navigate]);
   return <AccDetails />;
 };
 
